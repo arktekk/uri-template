5524531bcc3f4879371a53203d1a34161422e5ac
Øyvind Raddum Berg <elacin@gmail.com>
2015-07-11 02:11:12 -0500

    Ported to scala.js, dropped support for scala 2.10, bumped scala, sbt, scalatest

--- project/Build.scala
+++ project/Build.scala
@@ -1,42 +1,22 @@
-import sbt._
+import org.scalajs.sbtplugin.ScalaJSPlugin.autoImport._
 import sbt.Keys._
-import xml.Group
+import sbt._
+
+import scala.xml.Group
 
 object Build extends sbt.Build {
-
-  lazy val buildSettings = Defaults.defaultSettings ++ Seq(
-    organization := "no.arktekk",
-    scalaVersion := "2.11.2",
-    crossScalaVersions := Seq("2.11.2","2.10.1"),
-    publishTo <<= (version) apply {
-      (v: String) => if (v.trim().endsWith("SNAPSHOT")) Some(Resolvers.sonatypeNexusSnapshots) else Some(Resolvers.sonatypeNexusStaging)
+  val buildSettings = Defaults.coreDefaultSettings ++ Seq(
+    organization         := "no.arktekk",
+    scalaVersion         := "2.11.7",
+    publishTo           <<= isSnapshot {
+      case true  ⇒ Some("Sonatype Nexus Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots")
+      case false ⇒ Some("Sonatype Nexus Staging"   at "https://oss.sonatype.org/service/local/staging/deploy/maven2")
     },
     pomIncludeRepository := { x => false },
-    credentials += Credentials(Path.userHome / ".sbt" / "arktekk-credentials")      
+    credentials          += Credentials(Path.userHome / ".sbt" / "arktekk-credentials")
   )
 
-  lazy val root = Project(
-    id = "root",
-    base = file("."),
-    settings = buildSettings ++ Seq(
-      description := "URI Template",
-      name := "uri-template", 
-      libraryDependencies ++= Seq(
-        "org.scalatest" %% "scalatest" % "2.2.2" % "test"
-      ),
-      libraryDependencies <++= (scalaBinaryVersion) apply {(sv: String) =>
-        if(sv == "2.11") Seq("org.scala-lang.modules" % "scala-parser-combinators_2.11" % "1.0.2") else Nil
-      },
-    manifestSetting
-    ) ++ mavenCentralFrouFrou
-  )
-
-  object Resolvers {
-    val sonatypeNexusSnapshots = "Sonatype Nexus Snapshots" at "https://oss.sonatype.org/content/repositories/snapshots"
-    val sonatypeNexusStaging = "Sonatype Nexus Staging" at "https://oss.sonatype.org/service/local/staging/deploy/maven2"
-  }
-
-  lazy val manifestSetting = packageOptions <+= (name, version, organization) map {
+  val manifestSetting = packageOptions <+= (name, version, organization) map {
     (title, version, vendor) =>
       Package.ManifestAttributes(
         "Created-By" -> "Simple Build Tool",
@@ -53,7 +33,7 @@ object Build extends sbt.Build {
   }
 
   // Things we care about primarily because Maven Central demands them
-  lazy val mavenCentralFrouFrou = Seq(
+  val mavenCentralFrouFrou = Seq(
     homepage := Some(new URL("http://github.com/arktekk/uri-template")),
     startYear := Some(2011),
     licenses := Seq(("Apache 2", new URL("http://www.apache.org/licenses/LICENSE-2.0.txt"))),
@@ -72,4 +52,31 @@ object Build extends sbt.Build {
       </developers>
     )}
   )
+
+  val sourceMapTransform = (isSnapshot, version) map {
+    case (true, v) ⇒
+      val a = new java.io.File("").toURI.toString.replaceFirst("/$", "")
+      val g = s"https://raw.githubusercontent.com/arktekk/uri-template/$v"
+      s"-P:scalajs:mapSourceURI:$a->$g/"
+    case (false, _) ⇒ ""
+  }
+
+  val uriTemplate = crossProject.in(file("."))
+    .settings(buildSettings ++ mavenCentralFrouFrou :_*)
+    .settings(
+      name                 := "uri-template",
+      description          := "URI Template",
+      libraryDependencies  += "org.scalatest" %%% "scalatest" % "3.0.0-M6" % "test",
+      manifestSetting
+    ).jsSettings(
+      scalacOptions       <+= sourceMapTransform,
+      libraryDependencies  += "org.scala-js" %%% "scala-parser-combinators" % "1.0.2"
+    ).jvmSettings(
+      libraryDependencies  += "org.scala-lang.modules" %% "scala-parser-combinators" % "1.0.4"
+    )
+
+  val jvm = uriTemplate.jvm
+  val js  = uriTemplate.js
+
+  val root = project.aggregate(jvm, js).settings(publish := {}, publishLocal := {})
 }
--- project/plugins.sbt
+++ project/plugins.sbt
@@ -8,3 +8,5 @@ addSbtPlugin("com.github.mpeltonen" % "sbt-idea" % "1.6.0")
 resolvers += Resolver.url("sbt-plugin-releases", new URL("http://scalasbt.artifactoryonline.com/scalasbt/sbt-plugin-releases/"))(Resolver.ivyStylePatterns)
 
 addSbtPlugin("com.typesafe.sbt" % "sbt-pgp" % "0.8.3")
+
+addSbtPlugin("org.scala-js" % "sbt-scalajs" % "0.6.4") 
